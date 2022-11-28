dfc8f39c4438a7af0cb04662d3a77e4bbcfeb637
notelly <117340086+notelly@users.noreply.github.com>
2022-11-28 11:17:39 +0900

    Update Study.java

--- Study.java
+++ Study.java
@@ -112,38 +112,40 @@ public class Study {
 		5000 8
 		※ 입력에서 첫번째 줄은 총 금액, 두번째 줄은 물건 종류, 세번째부터 아래 내용은 해당 물건은 가격과 갯수를 의미함.*/
 		
-		int price;
-		int number;
-		String name;
-		Study[] arrys;
+		Info[] arrys;
+		int sum = 0;
 		
 		System.out.println("영수증 총 금액 입력 >");
 		int total = Integer.parseInt(sc.nextLine());
 		System.out.println("구매한 물품의 총 갯수 입력 >");
 		int mount = Integer.parseInt(sc.nextLine());
-		arrys = new Study[mount];
+		arrys = new Info[mount];
 		for(int i = 0; i < arrys.length; i++) {
-			arrys[i] = new 
-			Study st = new Study();
+			Info info = new Info();
 			System.out.println("구매한 물품의 이름, 가격과 갯수를 입력하세요.");
 			System.out.println("상품 이름 >");
-			String nm = sc.nextLine();
-			st.name = nm;
+			info.name = sc.nextLine();
 			
 			System.out.println("상품 가격 >");
-			pr = Integer.parseInt(sc.nextLine());
-			st.price = pr;
+			info.price = Integer.parseInt(sc.nextLine());
 			
-			System.out.println("상품 가격 >");
-			pr = Integer.parseInt(sc.nextLine());
-			st.price = pr;
+			System.out.println("상품 갯수 >");
+			info.number = Integer.parseInt(sc.nextLine());
+			
+			arrys[i] = info;
 		}
-		
+		for(int i = 0; i < arrys.length; i++) {
+			sum += (arrys[i].price * arrys[i].number);
+			System.out.println("상품 이름 : " + arrys[i].name);
+			System.out.println("상품 가격 : " + arrys[i].price);
+			System.out.println("상품 갯수 : " + arrys[i].number);
+			System.out.println("=====================");
+		}
+
 		if (total == sum) {
-			System.out.println(yes);
+			System.out.println("yes");
 		}else if (total != sum) {
-			System.out.println(no);
+			System.out.println("no");
 		}
-		
 	}
 }
